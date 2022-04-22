cc192c54ecf2b57cbfbf03bdd429d11c8eeadd97
PavelAbramoff <96954222+PavelAbramoff@users.noreply.github.com>
2022-04-22 09:32:17 +0400

    Initial Commit

---
+++ iOS-5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-5
+//
+//  Created by Pavel Абрамов on 22.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
